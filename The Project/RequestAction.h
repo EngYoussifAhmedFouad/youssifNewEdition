8c0a48df98e3e9aafa8cbe18ca2534a9c5e5aac1
EngYoussifAhmedFouad <youssiffouad2007@gmail.com>
2026-05-02 18:34:35 +0300

    action request edit constructors

--- The Project/RequestAction.h
+++ The Project/RequestAction.h
@@ -11,23 +11,47 @@ private:
     ORD_TYPE type;
     int size;
     double money;
-    
-
+	bool CanShare;
+	int Seats;
+	int Duration;
+	int Distance;
+    Order* pOrd;
 public:
     RequestAction(int id, ORD_TYPE typ, int sz, double mon, int time)
     {
+        Order* pOrd = new Order(OrderID, type, size, money, actionTime);
         OrderID = id;
         type = typ;
         size = sz;
         money = mon;
         actionTime = time;
     }
-
+    RequestAction(int id, ORD_TYPE typ, int sz, double mon, int time, int seats, int duration,bool canShare)
+    {
+        Order* pOrd = new Order(OrderID, type, size, money, actionTime);
+        OrderID = id;
+        type = typ;
+        size = sz;
+        money = mon;
+        actionTime = time;
+		Duration = duration;
+		CanShare = canShare;
+    }
+    RequestAction(int id, ORD_TYPE typ, int sz, double mon, int time, int distance)
+    {
+        Order* pOrd = new Order(OrderID, type, size, money, actionTime);
+		OrderID = id;
+		type = typ;
+		size = sz;
+		money = mon;
+		actionTime = time;
+		Distance = distance;
+    }
     virtual ~RequestAction() {}
 
     virtual void Act(Restaurant* pRest) override
     {
-         Order* pOrd = new Order(OrderID, type, size, money, actionTime);
+        
          pRest->AddOrder(pOrd); 
          
     }
